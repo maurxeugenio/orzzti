a6a9d37bffe72fa792d342673e4dabe73a42b120
maurx <maurx.dev@gmail.com>
2018-01-09 19:41:09 -0200

    analitcs

--- react/contact.js
+++ react/contact.js
@@ -95,29 +95,30 @@ var Contact = React.createClass({
         }else{
             if (this.state.sentIs==false) {
                 return(
-                    <div className="col s12 l6 m12">
-                    <h5 className="light">Entre em contato</h5>
-                    <div className="input-field col s12">
-                    <i className="material-icons prefix">account_circle</i>
-                    <input onChange={this.handleInputChange} id="icon_prefix" name='name' type="text" className="validate" data-length="40"/>
-                    <label htmlFor="icon_prefix">Seu nome</label>
+                    <div className='col s12 l6 m12'>
+                    <h5 className='light'>Entre em contato</h5>
+                    
+                    <div className='input-field col s12'>
+                    <i className='material-icons prefix'>account_circle</i>
+                    <input onChange={this.handleInputChange} id='icon_prefix' name='name' type='text' placeholder='Nome completo' className='validate' data-length='40'/>
+                    <label htmlFor='icon_prefix'>Seu nome</label>
                     </div>
-                    <div className="input-field col s12">
-                    <i className="material-icons prefix">email</i>
-                    <input onChange={this.handleInputChange} id="icon_email" name='email' type="email" className="validate" />
-                    <label htmlFor="icon_email" data-error="Êpa, êpa, êpa! Vamos corrigir isso aí!" data-success="Tudo certo!">Email</label>
+                    <div className='input-field col s12'>
+                    <i className='material-icons prefix'>email</i>
+                    <input onChange={this.handleInputChange} id='icon_email' name='email' type='email' placeholder='seu@email.com' className='validate' />
+                    <label htmlFor='icon_email' data-error='Êpa, êpa, êpa! Vamos corrigir isso aí!' data-success='Tudo certo!'>Email</label>
                     </div>
-                    <div className="input-field col s12">
-                    <i className="material-icons prefix">subject</i>
-                    <input onChange={this.handleInputChange} id="icon_email" name='subject' type="text" data-length="40" />
-                    <label htmlFor="icon_email">Assunto</label>
+                    <div className='input-field col s12'>
+                    <i className='material-icons prefix'>subject</i>
+                    <input onChange={this.handleInputChange} id='icon_email' name='subject' type='text' placeholder='Dúvidas, orçamentoes...' data-length='40' />
+                    <label htmlFor='icon_email'>Assunto</label>
                     </div>
-                    <div className="input-field col s12">
+                    <div className='input-field col s12'>
                     <i className="material-icons prefix">message</i>
-                    <textarea onChange={this.handleInputChange} id="icon_prefix2" name='message' className="materialize-textarea" data-length="300"></textarea>
-                    <label htmlFor="icon_prefix2">Sua mensagem</label>
+                    <textarea onChange={this.handleInputChange} id='icon_prefix2' name='message' className='materialize-textarea' placeholder='sua mensage...' data-length='300'></textarea>
+                    <label htmlFor='icon_prefix2'>Sua mensagem</label>
                     </div>
-                    <button onClick={this.handleClick} className="btn-large indigo darken-3 waves-effect" name="button">enviar <i className="material-icons right">send</i></button>
+                    <button onClick={this.handleClick} className='btn-large indigo darken-3 waves-effect' name='button'>enviar <i className='material-icons right'>send</i></button>
                     </div>
                 );
             }else{
